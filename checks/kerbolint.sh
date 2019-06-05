e8f4ffa59ced9d76d2cb368bdacb56df3c4bbfab
Trench <trench8891@gmail.com>
2019-06-04 23:56:39 -0400

    got line length working

--- checks/kerbolint.sh
+++ checks/kerbolint.sh
@@ -6,7 +6,7 @@ if [ -z "${scriptspath}" ]; then
   scriptspath="Script/"
 fi
 
-# maximum line length, use -1 to disable
+# maximum line length, use < 1 to disable
 maxlinelength=${MAX_LINE_LENGTH}
 if [ -z "${maxlinelength}" ]; then
   maxlinelength=60
@@ -25,30 +25,34 @@ function regerr() {
   errors+=("${error}")
 }
 
+numfiles=0
+
 # iterate over all scripts
 for script in $(find ${scriptpath} -name "*.ks"); do
-  # check line lengths
-  readarray lines < "${script}"
-  for i in ${!lines[@]}; do
-    line="${lines[${i}]}"
-    if [ ${#line} -gt ${maxlinelength} ]; then
-      let z=i+1
-      regerr ${script} ${z} "line is too long (${#line})"
-    fi
-  done
+  let numfiles=numfiles+1
+  if [ ${maxlinelength} -gt 0 ]; then
+    # check line lengths
+    readarray lines < "${script}"
+    for i in ${!lines[@]}; do
+      line="${lines[${i}]}"
+      if [ ${#line} -gt ${maxlinelength} ]; then
+        let z=i+1
+        regerr ${script} ${z} "line is too long (${#line})"
+      fi
+    done
+  fi
 
   # ensure all statements end with a period
 done
 
-# print out any errors
-if [ ${#errors[@]} -eq 0 ]; then
-  echo "no issues found"
-  exit 0
-else
-  for error in ${errors[@]}; do
-    echo "${error}"
-  done
-fi
+for error in "${errors[@]}"; do
+  echo "${error}"
+done
+echo "found ${#errors[@]} errors in ${numfiles} files"
 
-echo "linter in progress"
-exit 1
+# print out any errors
+if [ "${#errors[@]}" -gt 0 ]; then
+  exit 1
+else
+  exit 0
+fi
