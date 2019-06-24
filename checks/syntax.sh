4fa36c33226057422afb3f5682abef5aae451a7f
Noah Landau <trench8891@gmail.com>
2019-06-24 18:53:21 -0400

    this is going to take a while but making progress

--- checks/syntax.sh
+++ checks/syntax.sh
@@ -29,6 +29,9 @@ done
 
 shift $((OPTIND-1))
 
+# declare array to hold errors
+declare -a errors
+
 # count number of files examined
 num_files=0
 
@@ -36,12 +39,13 @@ num_files=0
 num_files=0
 
 # register an error
-function printerr() {
+function regerr() {
   script_path="${1}"
   linenum="${2}"
   message="${3}"
 
-  echo "${script_path}:${linenum} ${message}"
+  error="${script_path}:${linenum} ${message}"
+  errors+=("${error}")
 }
 
 # iterate over all scripts
@@ -51,12 +55,21 @@ for script in $(find ${scripts_path} -name "*.ks"); do
   lazyglobal="on"
   instruction_count=0
   mode="start"
+  breakflag="false"
 
   while read -r line; do
+    if [[ "${breakflag}" = "true" ]]; then
+      break
+    fi
+
     let line_num=line_num+1
     tail="${line}"
 
     while [[ -n "${tail}" ]]; do
+      if [[ "${breakflag}" = "true" ]]; then
+        break
+      fi
+
       if [[ "${tail}" =~ ^// ]]; then
         tail=""
       else
@@ -66,29 +79,55 @@ for script in $(find ${scripts_path} -name "*.ks"); do
               mode="directive"
               tail="${tail:1}"
             else
-              printerr ${script} ${line_num} "unknown command: $(echo "${tail}" | sed 's/[[:space:](:].*$//')"
-              exit 1
+              regerr ${script} ${line_num} "unknown command: $(echo "${tail}" | sed 's/[[:space:](:.].*$//')"
+              breakflag="true"
             fi
             ;;
           directive)
             if [[ ${instruction_count} -gt 0 ]]; then
-              printerr ${script} ${line_num} "compiler directives must precede commands"
-              exit 1
+              regerr ${script} ${line_num} "compiler directives must precede commands"
+              breakflag="true"
             else
-              if [[ "${tail}" =~ ^[Ll][Aa][Zz][Yy][Gg][Ll][Oo][Bb][Aa][Ll][[:space:]$] ]]; then
-                tail=$(echo "${tail}" | sed -E 's/^[Ll][Aa][Zz][Yy][Gg][Ll][Oo][Bb][Aa][Ll][[:space:]$]+//')
+              if [[ "${tail}" =~ ^[Ll][Aa][Zz][Yy][Gg][Ll][Oo][Bb][Aa][Ll][[:space:]$.] ]]; then
+                tail=$(echo "${tail}" | sed -E 's/^[Ll][Aa][Zz][Yy][Gg][Ll][Oo][Bb][Aa][Ll][[:space:]$.]+//')
                 mode="lazyGlobal"
               else
-                printerr ${script} ${line_num} "unknown compiler directive: $(echo "${tail}" | sed 's/[[:space:]].*$//')"
-                exit 1
+                regerr ${script} ${line_num} "unknown compiler directive: $(echo "${tail}" | sed 's/[[:space:]].*$//')"
+                breakflag="true"
               fi
             fi
             ;;
+          lazyGlobal)
+            if [[ "${tail}" =~ ^[Oo][Ff][Ff][[:space:]$.] ]]; then
+              lazyglobal="off"
+              tail=$(echo "${tail}" | sed -E 's/^[Oo][Ff][Ff][[:space:]$.]+//')
+              mode="expectEnd"
+            elif [[ "${tail}" =~ ^[Oo][Nn][[:space:]$.] ]]; then
+              lazyglobal="on"
+              tail=$(echo "${tail}" | sed -E 's/^[Oo][Nn][[:space:]$.]+//')
+              mode="expectEnd"
+            else
+              regerr ${script} ${line_num} "invalid lazy global mode: $(echo "${tail}" | sed 's/[[:space:](:.].*$//')"
+              breakflag="true"
+            fi
+            ;;
           *)
-            echo "unknown mode: ${mode}"
-            exit 1
+            regerr ${script} ${line_num} "unknown mode: ${mode}"
+            breakflag="true"
         esac
       fi
     done
   done < "${script}"
 done
+
+# print out any errors
+for error in "${errors[@]}"; do
+  echo "${error}"
+done
+echo "found errors in ${num_files} files"
+
+if [ "${#errors[@]}" -gt 0 ]; then
+  exit 1
+else
+  exit 0
+fi
