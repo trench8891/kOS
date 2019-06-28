66fce2fa8f50cfbd9a357b00e103bd3c2bddd214
Noah Landau <trench8891@gmail.com>
2019-06-27 21:41:19 -0400

    foos ro dah

--- checks/syntax.sh
+++ checks/syntax.sh
@@ -4,9 +4,6 @@ set -e
 
 scripts_path="Script"
 
-identifier_regex="^[_a-zA-Z][_a-zA-Z0-9]*$"
-suffix_separator=":"
-
 usage_message="Usage: $(basename "${0}") [-h] [-p scripts_path]
 
 Options:
@@ -35,9 +32,6 @@ declare -a errors
 # count number of files examined
 num_files=0
 
-# count number of files examined
-num_files=0
-
 # register an error
 function regerr() {
   script_path="${1}"
@@ -46,6 +40,8 @@ function regerr() {
 
   error="${script_path}:${linenum} ${message}"
   errors+=("${error}")
+
+  breakflag=true
 }
 
 # iterate over all scripts
@@ -73,47 +69,54 @@ for script in $(find ${scripts_path} -name "*.ks"); do
       if [[ "${tail}" =~ ^// ]]; then
         tail=""
       else
+        # echo "${script}:${line_num}:${mode} ${tail}"
         case ${mode} in
           start)
             if [[ "${tail}" =~ ^@ ]]; then
               mode="directive"
               tail="${tail:1}"
             else
-              regerr ${script} ${line_num} "unknown command: $(echo "${tail}" | sed 's/[[:space:](:.].*$//')"
-              breakflag="true"
+              regerr ${script} ${line_num} "unknown command: $(echo "${tail}" | sed 's/[^_[:alnum:]].*$//')"
             fi
             ;;
           directive)
             if [[ ${instruction_count} -gt 0 ]]; then
               regerr ${script} ${line_num} "compiler directives must precede commands"
-              breakflag="true"
             else
-              if [[ "${tail}" =~ ^[Ll][Aa][Zz][Yy][Gg][Ll][Oo][Bb][Aa][Ll][[:space:]$.] ]]; then
-                tail=$(echo "${tail}" | sed -E 's/^[Ll][Aa][Zz][Yy][Gg][Ll][Oo][Bb][Aa][Ll][[:space:]$.]+//')
+              if [[ "${tail}" =~ ^[Ll][Aa][Zz][Yy][Gg][Ll][Oo][Bb][Aa][Ll][^_[:alnum:]]? ]]; then
+                tail=$(echo "${tail}" | sed -E 's/^[Ll][Aa][Zz][Yy][Gg][Ll][Oo][Bb][Aa][Ll][[:space:]]*//')
                 mode="lazyGlobal"
               else
-                regerr ${script} ${line_num} "unknown compiler directive: $(echo "${tail}" | sed 's/[[:space:]].*$//')"
-                breakflag="true"
+                regerr ${script} ${line_num} "unknown compiler directive: $(echo "${tail}" | sed 's/[^_[:alnum:]].*$//')"
               fi
             fi
             ;;
           lazyGlobal)
-            if [[ "${tail}" =~ ^[Oo][Ff][Ff][[:space:]$.] ]]; then
+            if [[ "${tail}" =~ ^[Oo][Ff][Ff][^_[:alnum:]]? ]]; then
               lazyglobal="off"
-              tail=$(echo "${tail}" | sed -E 's/^[Oo][Ff][Ff][[:space:]$.]+//')
-              mode="expectEnd"
-            elif [[ "${tail}" =~ ^[Oo][Nn][[:space:]$.] ]]; then
+              tail=$(echo "${tail}" | sed -E 's/^[Oo][Ff][Ff][[:space:]]*//')
+              mode="expectPeriod"
+            elif [[ "${tail}" =~ ^[Oo][Nn][^_[:alnum:]]? ]]; then
               lazyglobal="on"
-              tail=$(echo "${tail}" | sed -E 's/^[Oo][Nn][[:space:]$.]+//')
-              mode="expectEnd"
+              tail=$(echo "${tail}" | sed -E 's/^[Oo][Nn][[:space:]]*//')
+              mode="expectPeriod"
+            elif [[ "${tail}" =~ ^[^_[:alnum:]] ]]; then
+              regerr ${script} ${line_num} "unexpected separator: \"${tail:0:1}\""
             else
-              regerr ${script} ${line_num} "invalid lazy global mode: $(echo "${tail}" | sed 's/[[:space:](:.].*$//')"
-              breakflag="true"
+              regerr ${script} ${line_num} "invalid lazy global mode: $(echo "${tail}" | sed 's/[^_[:alnum:]].*$//')"
+            fi
+            ;;
+          expectPeriod)
+            if [[ "${tail}" =~ ^\. ]]; then
+              mode="start"
+              tail="${tail:1}"
+            else
+              regerr ${script} ${line_num} "missing period"
             fi
             ;;
           *)
+            echo "${tail}"
             regerr ${script} ${line_num} "unknown mode: ${mode}"
-            breakflag="true"
         esac
       fi
     done
